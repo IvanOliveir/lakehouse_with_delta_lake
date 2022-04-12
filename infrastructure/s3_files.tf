c4f61101f338383c1763b57590a71b2f7b39da90
ivan.oliveira <ivanoliveira123321@gmail.com>
2022-04-12 17:29:50 -0300

    changed filemd5

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -10,13 +10,13 @@ resource "aws_s3_bucket_object" "delta_insert" {
   bucket = "${aws_s3_bucket.lakehouse.id}"
   key = "emr-code/pyspark/01_delta_spark_insert.py"
   source = "../etl/01_delta_spark_insert.py"
-  etag = filemd5("../etl/01_delta_spark_insert.py")
+  #etag = filemd5("../etl/01_delta_spark_insert.py")
 }
 
 resource "aws_s3_bucket_object" "delta_upsert" {
   bucket = "${aws_s3_bucket.lakehouse.id}"
   key = "emr-code/pyspark/02_delta_spark_upsert.py"
   source = "../etl/02_delta_spark_upsert.py"
-  etag = filemd5("../etl/02_delta_spark_upsert.py")
+  #etag = filemd5("../etl/02_delta_spark_upsert.py")
 }
 
