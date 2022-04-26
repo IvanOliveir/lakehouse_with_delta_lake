25f76d541bcd2bf934068bb995a8bcd77accc122
ivan.oliveira <ivanoliveira123321@gmail.com>
2022-04-26 18:11:43 -0300

    sobe infra airflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,3 +6,15 @@ variable "aws_region" {
 variable "lambda_function_name" {
   default = "executaEMR"
 }
+
+variable "key_pair_name" {
+  default = 'ivan-test'
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-0c65bc0388b3b9ade"
+}
+
+variable "vpc_id" {
+  default = "vpc-035fbb438082e80c1"
+}
