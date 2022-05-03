8a5fc89bf70bca73619f0658764e55f82b3bf0c3
ivan.oliveira <ivanoliveira123321@gmail.com>
2022-05-03 18:21:14 -0300

    changed lambda policie's name

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -18,7 +18,7 @@ EOF
 }
 
 resource "aws_iam_policy" "lambda" {
-  name        = "LambdaBasicExecutionRolePolicy"
+  name        = "IvanTestLambdaBasicExecutionRolePolicy"
   path        = "/"
   description = "Provides write permissions to CloudWatch Logs, S3 buckets and EMR Steps"
 
